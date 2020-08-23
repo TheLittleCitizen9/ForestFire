3bbd12580729496113286d1a91bf7ac60a4fc596
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-23 21:00:36 +0300

    Started implementing the Tree class

---
+++ ForestFire/ForestFire/Tree.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForestFire
+{
+    public class Tree
+    {
+        public int Health { get; set; }
+        public TreeStatus TreeStatus { get; set; }
+        public Tree RelatedTo { get; set; }
+        public event Action RelatedTrees;
+
+        public Tree(int health, TreeStatus status, Tree relatedTo = null)
+        {
+            Health = health;
+            TreeStatus = status;
+            RelatedTo = relatedTo;
+        }
+
+        public void RelateToTree()
+        {
+            RelatedTo.RelatedTrees += ChangeStatus;
+        }
+
+        public void ChangeTreeStatus()
+        {
+            RelatedTrees?.Invoke();
+        }
+
+        public void BurnTree()
+        {
+            TreeStatus = TreeStatus.Burning;
+            Health--;
+            if(Health == 0)
+            {
+                TreeStatus = TreeStatus.Dead;
+            }
+        }
+
+        public void ChangeStatus()
+        {
+            switch (TreeStatus)
+            {
+                case TreeStatus.Healthy:
+                    BurnTree();
+                    break;
+                case TreeStatus.Burning:
+                    if(Health == 0)
+                    {
+                        TreeStatus = TreeStatus.Dead;
+                    }
+                    break;
+                case TreeStatus.Dead:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ ForestFire/ForestFire/TreeStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForestFire
+{
+    public enum TreeStatus
+    {
+        Healthy,
+        Burning,
+        Dead
+    }
+}
